5fce4dd2ffb351200f1c3c8d3f4494f820b7c69d
Ngueoko <ngueokothomas@gmail.com>
2026-06-28 13:39:37 +0200

    ajouts de la fonction de recherche et de l'edition du produit

--- src/main/java/com/cdi/bdccspringmvc/repository/ProductRepository.java
+++ src/main/java/com/cdi/bdccspringmvc/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.cdi.bdccspringmvc.repository;
 import com.cdi.bdccspringmvc.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByNameContainsIgnoreCase(String keyword);
 }
--- src/main/java/com/cdi/bdccspringmvc/web/ProductController.java
+++ src/main/java/com/cdi/bdccspringmvc/web/ProductController.java
@@ -21,54 +21,74 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-   @GetMapping("/user/index")
-   @PreAuthorize("hasRole('USER')")
-   public String index(Model model){
+    @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
+    public String index(@RequestParam(defaultValue = "") String keyword, Model model) {
+        List<Product> productList;
+        productList = keyword.length() > 0 ?
+                productRepository.findByNameContainsIgnoreCase(keyword) :
+                productRepository.findAll();
 
-   List<Product> productList=productRepository.findAll();
-     model.addAttribute("productList",productList);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("productList", productList);
         return "products";
     }
+
     @GetMapping("/")
-    public String home(){
-        return"redirect:/user/index";
+    public String home() {
+        return "redirect:/user/index";
     }
+
     @GetMapping("/admin/delete")
     @PreAuthorize("hasRole('ADMIN')")
-    public String delete(@RequestParam Long id){
-       System.out.println(" on delete : ");
-       productRepository.deleteById(id);
-       return"redirect:/user/index";
+    public String delete(@RequestParam Long id) {
+        System.out.println(" on delete : ");
+        productRepository.deleteById(id);
+        return "redirect:/user/index";
     }
+
     @GetMapping("/admin/newProduct")
     @PreAuthorize("hasRole('ADMIN')")
-    public String newProduct(Model model){
-       System.out.println("  Voici ......................................................................");
+    public String newProduct(Model model) {
+        System.out.println("  Voici ......................................................................");
         model.addAttribute("product", new Product());
-        return"new-product";
+        return "new-product";
     }
+
+    @GetMapping("/admin/edit")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String editProduct(Long id, Model model) {
+        System.out.println("  Voici EDIT 1 ......................................................................");
+        Product product = productRepository.getReferenceById(id);
+        model.addAttribute("product", product);
+
+        return "new-product";
+    }
+
     @PostMapping("/admin/saveProduct")
     @PreAuthorize("hasRole('ADMIN')")
-    public String saveProduct(@Valid Product product, BindingResult bindingResult,Model model){
-       if(bindingResult.hasErrors()){
-           return "new-product";
-       }
-       System.out.println("Product "+product.toString());
-       productRepository.save(product);
-       return"redirect:/user/index";
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "new-product";
+        }
+
+        productRepository.save(product);
+        return "redirect:/user/index";
     }
 
     @GetMapping("/notAuthorized")
-    public String notAuthorized(){
-         return"notAuthorized";
+    public String notAuthorized() {
+        return "notAuthorized";
     }
+
     @GetMapping("/login")
-    public String login(){
-        return"login";
+    public String login() {
+        return "login";
     }
+
     @GetMapping("/logout")
-    public String logout(HttpSession session){
-       session.invalidate();
-        return"logout";
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "logout";
     }
 }
